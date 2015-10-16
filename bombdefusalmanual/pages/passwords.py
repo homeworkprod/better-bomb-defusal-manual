7f2cddecc03008923e48408875e8b15a5fb20f3e
Jochen Kupperschmidt <homework@nwsnet.de>
2015-10-16 14:32:35 +0200

    Added unused letters to passwords page.

--- bombdefusalmanual/pages/passwords.py
+++ bombdefusalmanual/pages/passwords.py
@@ -9,6 +9,7 @@ On the Subject of Passwords
 
 from itertools import chain, groupby
 from operator import itemgetter
+from string import ascii_lowercase
 
 from ..subjects.passwords import PASSWORDS
 from ..templating import render_template
@@ -35,12 +36,19 @@ def get_unique_letters(letters_by_position):
     return frozenset(chain.from_iterable(letters_by_position))
 
 
+def get_unused_letters():
+    used_letters = frozenset(chain.from_iterable(PASSWORDS))
+    return frozenset(ascii_lowercase).difference(used_letters)
+
+
 def render_page():
     passwords_by_first_letter = group_passwords_by_first_letter()
     letters_by_position = get_letters_by_position()
     unique_letters = get_unique_letters(letters_by_position)
+    unused_letters = get_unused_letters()
 
     return render_template('passwords',
                            passwords_by_first_letter=passwords_by_first_letter,
                            letters_by_position=letters_by_position,
-                           unique_letters=unique_letters)
+                           unique_letters=unique_letters,
+                           unused_letters=unused_letters)
