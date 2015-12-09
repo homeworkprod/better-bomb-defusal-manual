4173c5401c9c84b41354f3aa211eadb84553d0fe
Jochen Kupperschmidt <homework@nwsnet.de>
2015-12-09 05:19:37 +0100

    Allow injection of custom template filters.

--- bombdefusalmanual/templating.py
+++ bombdefusalmanual/templating.py
@@ -13,14 +13,26 @@ from jinja2 import Environment, PackageLoader, StrictUndefined
 FILENAME_EXTENSION = '.html'
 
 LOADER = PackageLoader('bombdefusalmanual.pages')
-ENV = Environment(loader=LOADER, undefined=StrictUndefined)
 
 
-def render_template(name, **context):
-    template = load_template(name)
+def render_template(name, *, custom_filters=None, **context):
+    template = load_template(name, custom_filters=custom_filters)
+
     return template.render(**context)
 
 
-def load_template(name):
+def load_template(name, *, custom_filters=None):
+    env = create_environment(custom_filters=custom_filters)
+
     filename = name + FILENAME_EXTENSION
-    return ENV.get_template(filename)
+
+    return env.get_template(filename)
+
+
+def create_environment(*, custom_filters=None):
+    env = Environment(loader=LOADER, undefined=StrictUndefined)
+
+    if custom_filters is not None:
+        env.filters.update(custom_filters)
+
+    return env
